bbc7bfdb23b8058ec463177ee8c97574312ed945
Annedrew <1196244890@qq.com>
2024-01-23 10:05:08 +0100

    Add confirm prompt

--- react-review/src/components/Todo.js
+++ react-review/src/components/Todo.js
@@ -1,6 +1,12 @@
+import { useState } from 'react'
+import Modal from './Modal'
+import Backdrop from './Backdrop'
+
 function Todo(props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    
     function deleteHandler(){
-        console.log(props.text)
+        setModalIsOpen(true)
     }
 
     return(
@@ -9,6 +15,9 @@ function Todo(props) {
             <div className='actions'>
                 <button className='btn' onClick={ deleteHandler }>Delete</button>
             </div>
+            {/* { modalIsOpen ? <Modal /> : null } */}
+            { modalIsOpen && <Modal /> }
+            { modalIsOpen && <Backdrop /> }
         </div>
     );
 }
